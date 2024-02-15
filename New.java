30d77feae77e8159e01c3b432d5d59aadf984f35
rohanongithub <151556906+rohanongithub@users.noreply.github.com>
2024-02-15 18:17:53 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,45 @@
+//Creation of object and class
+
+class Create{
+    public int newObject;
+
+    void newmethod(){
+       System.out.println("Hey!!");
+    };
+}
+// public class New {
+//     public static void main(String[]args){
+//         Create my = new Create();
+// my.newObject = 10;
+//         System.out.println(my.newObject);
+//     }
+// }
+
+/**
+ * Newclass
+ */
+class Newclass {
+int number;
+String name;
+int xyz;
+
+    
+} 
+class New{
+    public static void main(String[]args){
+      
+        Newclass obj = new Newclass();
+        obj.number=12323;
+        obj.name="Rohan";
+        System.out.println(obj.name);
+        System.out.println(obj.number);
+
+
+Newclass o1 = new Newclass();
+o1.xyz = 123;
+System.out.println(o1.xyz);
+
+    }
+}
+
+
